5a226f0518d45ca87ce37c431a987e7445d2157a
Роман Буцких <rombik44@yandex.ru>
2023-11-21 22:26:27 +0300

    feat: сделал первую миграцию

--- cherry/settings.py
+++ cherry/settings.py
@@ -97,7 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
