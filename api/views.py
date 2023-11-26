f063f306dc120b76886c66db7a291b1c381fd74c
Наталья Арлазарова <sic15@yandex.ru>
2023-11-26 20:36:01 +0300

    добавлена фильтрация по категориям

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from products.models import Category, ChainStore, Product, Store
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 from api.serializers import (CategorySerializer, ChainStoreSerializer,
                              ProductSerializer, StoreLongSerializer,
@@ -10,6 +11,9 @@ from api.serializers import (CategorySerializer, ChainStoreSerializer,
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = PageNumberPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('category', 'stores')
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
