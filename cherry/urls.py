19570418bacd109b07eee19f319cb401a8b191fd
valerycode <lera200@yandex.ru>
2023-11-27 22:05:46 +0300

    Add urls for app users

--- cherry/urls.py
+++ cherry/urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
