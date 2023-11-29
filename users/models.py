6bb8fadf091ac08aee91b0d6c31e2c911a02b242
Роман Буцких <rombik44@yandex.ru>
2023-11-29 20:20:00 +0300

    feat: User model добавил поле фото, required_field, email_field

--- users/models.py
+++ users/models.py
@@ -3,29 +3,37 @@ from django.db import models
 
 
 class User(AbstractUser):
+    REQUIRED_FIELDS = ('phone', 'role', 'foto', 'first_name', 'last_name')
+    EMAIL_FIELD = 'username'
     ROLE = [
         ('m', 'marketer'),
         ('s', 'shopper'),
     ]
     username = models.EmailField(
-        unique=True,
         verbose_name='email',
     )
-    phone = models.CharField(max_length=12, verbose_name='Телефон')
+    phone = models.CharField(
+        max_length=12,
+        blank=True,
+        null=True,
+        verbose_name='Телефон'
+    )
     role = models.CharField(
         max_length=1,
         choices=ROLE,
         default='s',
         verbose_name='Роль пользователя приложения',
     )
-    # foto = models.ImageField(
-    #     upload_to='users/',
-    #     verbose_name='Фото пользователя',
-    # )
+    foto = models.ImageField(
+        upload_to='users/',
+        verbose_name='Фото пользователя',
+    )
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
     def __str__(self):
+        if self.first_name and self.last_name:
+            return self.get_full_name()
         return self.username
