731e0ad317404e6e294408d771123c0c8eb90fda
Савелий Худяк <hudyakss@gmail.com>
2023-11-30 22:18:51 +0500

    feat: products/favorites для авторизованных пользователей

--- api/views.py
+++ api/views.py
@@ -4,8 +4,7 @@ from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
-from api.serializers import (CategorySerializer, ChainStoreSerializer,
-                             ProductSerializer, StoreSerializer)
+from api.serializers import (CategorySerializer, ChainStoreSerializer, ProductSerializer, StoreSerializer)
 from products.models import Category, ChainStore, Favorites, Product, Store
 
 
@@ -13,27 +12,24 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def get_queryset(self):
+        if self.action == "favorites":
+            return Product.objects.filter(id__in=Favorites.objects.filter(user=self.request.user).values('product_id'))
+        return Product.objects.all()
+
     @action(detail=True, methods=['post', 'delete'])
     def favorite(self, request, pk=None):
         user = request.user
         product = get_object_or_404(Product, id=pk)
         if request.method == 'POST':
             Favorites.objects.get_or_create(user=user, product=product)
-            return Response('Товар успешно добавлен в избранное',
-                            status.HTTP_201_CREATED)
+            return Response('Товар успешно добавлен в избранное', status.HTTP_201_CREATED)
         user.favorites.filter(product=product).delete()
-        return Response('Товар успешно удален из избранного',
-                        status.HTTP_204_NO_CONTENT)
+        return Response('Товар успешно удален из избранного', status.HTTP_204_NO_CONTENT)
 
-    # TODO
-    # новый ендпоинт favorites со списком всех избранных товаров
-    # @action(detail=False, methods=['get',])
-    # def favorites(self, request, pk=None):
-    #     user = request.user
-    #     if not user.is_authenticated:
-    #         return Response('Недоступно неавторизованным пользователям', status.HTTP_401_UNAUTHORIZED)
-    #     else:
-    #         self.queryset = Product.objects.all()
+    @action(detail=False, methods=['get',])
+    def favorites(self, request):
+        return super().list(request)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
