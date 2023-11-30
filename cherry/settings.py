1bf1c8ed9c9622055965e1b4d453d94ea57ac485
Арина Сухова <itisarine@yandex.ru>
2023-11-30 22:08:34 +0300

    Добавлена возможность оставлять отзыв на товар

--- cherry/settings.py
+++ cherry/settings.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'dgfdg'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
 AUTH_USER_MODEL = 'users.User'
 
