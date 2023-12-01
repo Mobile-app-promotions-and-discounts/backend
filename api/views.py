f9b880304ab0b45541ec711cb2db39fd06835eac
valerycode <lera200@yandex.ru>
2023-12-01 11:57:51 +0300

    Delete unnecessary import

--- api/views.py
+++ api/views.py
@@ -2,7 +2,6 @@ from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
 
 from api.serializers import (CategorySerializer, ChainStoreSerializer,
                              ProductSerializer, ReviewSerializer,
