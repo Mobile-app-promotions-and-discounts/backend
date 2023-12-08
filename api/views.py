0d3ee9327e25f816a7b39b051800d5598c1c7517
valerycode <lera200@yandex.ru>
2023-12-08 16:53:46 +0300

    Improve code style to fix linters errors

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api.serializers import (CategorySerializer, ChainStoreSerializer,
