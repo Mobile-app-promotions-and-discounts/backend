bd66409709e620a6edaa14383cebb7fdb74c781e
Роман Буцких <rombik44@yandex.ru>
2023-12-25 13:17:53 +0300

    feat: создал команду добавления категорий товаров из вариантов выбора

---
+++ parsing_stores/management/commands/add_categories.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+
+from products.models import Category
+
+
+class Command(BaseCommand):
+    help = 'Создание категорий товаров из вариантов выбора.'
+
+    def handle(self, *args, **options):
+        category_names = [category[0] for category in Category.CategoryType.choices]
+        for category_name in category_names:
+            Category.objects.get_or_create(name=category_name)
+            # if not Category.objects.filter(name=category_name).exists():
+
