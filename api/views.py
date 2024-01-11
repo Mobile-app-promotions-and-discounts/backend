a148c0ab3a1b7268032ee18e0be19993eba60d56
valerycode <lera200@yandex.ru>
2024-01-11 23:39:28 +0600

    Delete unused import in api views

--- api/views.py
+++ api/views.py
@@ -5,7 +5,6 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api.serializers import (CategorySerializer, ChainStoreSerializer,
