00c3a39015e6b294957471bd2eed20b2272086fe
valerycode <lera200@yandex.ru>
2024-01-11 15:50:21 +0600

    Add django.contrib.postgres in INSTALLED_APPS for full text search in products list

--- cherry/settings.py
+++ cherry/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'rest_framework',
     'rest_framework_simplejwt',
     'djoser',
