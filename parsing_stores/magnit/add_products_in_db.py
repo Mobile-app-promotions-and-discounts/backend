ea152f826d53f0a55e3d9b81559ca66d6d53b354
Роман Буцких <rombik44@yandex.ru>
2024-02-04 21:25:58 +0300

    feat: добавил создание товаров в магазине, ломается на дубликатах продуктов, созданных через bulk_create

--- parsing_stores/magnit/add_products_in_db.py
+++ parsing_stores/magnit/add_products_in_db.py
@@ -1,13 +1,11 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
+
+from django.core.files.base import ContentFile
+from django.shortcuts import get_object_or_404
 
 from parsing_stores.magnit.async_magnit_parsing import run_get_data_in_stores
 from products.models import Product, Category, Discount, ProductsInStore, Store
-
-
-PRODUCT_INDEX = 0
-DISCOUNT_INDEX = 1
-PRICE_INDEX = 2
-STORE_INDEX = 3
+from parsing_stores.magnit.decorators import calc_time_work
 
 
 def create_products_obj(data: List[Dict[str, str | bytes | None]]) -> List[Product]:
@@ -20,23 +18,78 @@ def create_products_obj(data: List[Dict[str, str | bytes | None]]) -> List[Produ
     ]
 
 
-def create_discounts_obj(data: List[Dict[str, str]]) -> List[Discount]:
-    """Создание объектов скидки."""
-    return [Discount(**discount) for discount in data]
-
-
-def create_products_in_store_obj(data: List[Dict[str, str]]) -> List[ProductsInStore]:
-    pass
-
-
-def split_data(data: List[List[Dict[str, str | bytes | None], Dict[str, str], Dict[str, str]]]) -> List[Dict, Dict, Dict]:
-    products_data = []
-    discounts_data = []
-    products_in_store_data = []
+@calc_time_work
+def create_products_and_discounts_obj(data: List[Dict[str, str | bytes | None]]) -> Tuple[List[Product], List[Discount]]:
+    """Создание списков объектов продуктов и скидок."""
+    products = []
+    discounts = []
     for item in data:
-        category = Category.objects.get(name=item[PRODUCT_INDEX].pop('category'))
-        item[PRODUCT_INDEX]['category'] = category
-        products_data.append(item[PRODUCT_INDEX])
-        discounts_data.append(item[DISCOUNT_INDEX])
-        
+        # print(item.get('category'))
+        category = Category.objects.get(name=item.get('category'))
+        name = item.get('name')
+        if not Product.objects.filter(name=name).exists():
+            product = Product(
+                name=name,
+                category=category,
+                barcode=item.get('barcode'),
+                main_image=ContentFile(item.get('image'), name=item.get('image_name')),
+            )
+            if product not in products:
+                products.append(product)
+        discount_rate = item.get('discount_rate') if item.get('discount_rate') else -1
+        discount_start = item.get('discount_start')
+        discount_end = item.get('discount_end')
+        if not Discount.objects.filter(discount_rate=discount_rate, discount_start=discount_start, discount_end=discount_end).exists():
+            discount = Discount(
+                discount_rate=discount_rate,
+                discount_start=discount_start,
+                discount_end=discount_end,
+            )
+            discounts.append(discount)
+    return products, discounts
 
+
+def create_products_in_stores_obj(data: List[Dict[str, str | bytes | None]]) -> List[ProductsInStore]:
+    """Создание объектов продуктов в магазинах."""
+    products_in_stores = []
+    for item in data:
+        product = get_object_or_404(Product, name=item.get('name'))
+        store = Store.objects.get(id_in_chain_store=item.get('id_in_chain_store'))
+        discount = Discount.objects.get(
+            discount_rate=item.get('discount_rate'),
+            discount_start=item.get('discount_start'),
+            discount_end=item.get('discount_end'),
+        )
+        initial_price = item.get('initial_price', False)
+        promo_price = item.get('promo_price', False)
+        if initial_price and promo_price:
+            products_in_stores.append(
+                ProductsInStore(
+                    product=product,
+                    store=store,
+                    discount=discount,
+                    initial_price=initial_price,
+                    promo_price=promo_price,
+                )
+            )
+    return products_in_stores
+
+
+@calc_time_work
+def add_in_db(products: List[Product], discounts: List[Discount]):
+    """Добавление данных в БД."""
+    add_products = Product.objects.bulk_create(products)
+    add_discounts = Discount.objects.bulk_create(discounts)
+    # add_products_in_store = ProductsInStore.objects.bulk_create(products_in_stores)
+    return add_products, add_discounts
+
+
+@calc_time_work
+def run_add_data_in_db():
+    data_in_stores = run_get_data_in_stores()
+    products, discounts = create_products_and_discounts_obj(data_in_stores)
+    add_products, add_discounts = add_in_db(products, discounts)
+    products_in_stores = create_products_in_stores_obj(data_in_stores)
+    add_pr_in_stores = ProductsInStore.objects.bulk_create(products_in_stores)
+    print(f'Добавлено продуктов {len(add_products)}\nДобавлено акций {len(add_discounts)}\nДобавлено продуктов в магазине {len(add_pr_in_stores)}')
+    
--- parsing_stores/magnit/async_magnit_parsing.py
+++ parsing_stores/magnit/async_magnit_parsing.py
@@ -27,7 +27,7 @@ async def get_data_in_url(session, request_setting):
 
 async def _get_image(session, url):
     async with session.get(url) as result:
-        print('Начало запроса')
+        # print('Начало запроса')
         return await result.read()
 
 
@@ -37,24 +37,25 @@ def set_params(params, change_param, value_param):
 
 
 def parse_data_product(data, data_keys):
-    discount = {}
-    product = {}
-    price = {}
+    # discount = {}
+    products = {}
+    # price = {}
     for out_key, in_key in data_keys:
-        if 'discount' in out_key:
-            discount[out_key] = data.get(in_key)
-        elif 'price' in out_key:
-            price[out_key] = data.get(in_key)
-        elif 'category' in out_key:
+        # if 'discount' in out_key:
+        #     discount[out_key] = data.get(in_key)
+        # elif 'price' in out_key:
+        #     price[out_key] = data.get(in_key)
+        if 'category' in out_key:
             name_category = data.get(in_key)
             for key, value in PARSING_MAGNIT.get('CATEGORIES').items():
                 if name_category in value:
-                    product[out_key] = key
-            if not product.get(out_key, False):
-                product[out_key] = 'DIFFERENT'
+                    products[out_key] = key
+            if not products.get(out_key, False):
+                products[out_key] = 'DIFFERENT'
         else:
-            product[out_key] = data.get(in_key)
-    return [product, discount, price]
+            products[out_key] = data.get(in_key)
+    # return [product, discount, price]
+    return products
 
 
 def get_image_name(st: str) -> str:
@@ -78,15 +79,16 @@ async def get_product_in_stores(request_settings, ids_store):
 
 
 async def get_images(products: List[List[Dict[str, str | None]]]) -> List[bytes]:
-    urls = [product[0].pop('image_url') for product in products]
+    urls = [product.get('image_url') for product in products]
     async with ClientSession() as session:
         reqs = [_get_image(session, url) for url in urls]
         results = await asyncio.gather(*reqs, return_exceptions=True)
     for product, image in zip(products, results):
+        product['image_name'] = get_image_name(product.pop('image_url'))
         if isinstance(image, Exception):
-            product[0]['image'] = None
+            product['image'] = None
         else:
-            product[0]['image'] = image
+            product['image'] = image
     return products
 
 
@@ -102,7 +104,7 @@ def run_get_data_in_stores():
             # r[store] = [parse_data_product(d, PARSING_MAGNIT.get('KEYS')) for d in products_store.get('data')]
             for product in products_store.get('data'):
                 pr_data = parse_data_product(product, PARSING_MAGNIT.get('KEYS'))
-                pr_data.append(dict(id_in_chain_store=store))
+                pr_data['id_in_chain_store'] = store
                 r.append(pr_data)
         # else:
         #     вставить лог что магазин не был опрошен
@@ -112,5 +114,6 @@ def run_get_data_in_stores():
     # with open('products_in_magnit.json', 'w') as file:
     #     file.write(json.dumps(r))
     print(f'Время работы {datetime.today() - start}')
+    # print(products[0])
     return products
 # pprint(asyncio.run(main()))
