8c58b2923d612da68af496f28e928bc1e63f3d83
Сергей Тыртычный <tyrtychnyy90@mail.ru>
2024-02-26 01:45:24 +0300

    Test

--- api/tasks.py
+++ api/tasks.py
@@ -10,15 +10,13 @@ from cherry.settings import EMAIL_HOST_USER
 def send_email(name: str, email: str, message: str, image_file=None) -> None:
     email = EmailMessage(
         subject=f'Обратная связь от пользователя {name}',
-        body=f'''Email пользователя: {email},
-Cообщение: {message}
-''',
+        body=f'Email пользователя: {email}, Cообщение: {message}',
         from_email=EMAIL_HOST_USER,
         to=[EMAIL_HOST_USER],
     )
     if image_file:
         format, imgstr = image_file.split(';base64,')
         ext = format.split('/')[-1]
-        file = MIMEImage(base64.b64decode(imgstr), _subtype=ext)
+        file = MIMEImage(base64.b64decode(imgstr), name=f'image.{ext}', _subtype=ext)
         email.attach(file)
     email.send(fail_silently=False)
