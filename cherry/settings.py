276c02cf356be62464fd38f690b4cb0e0ca0308b
Сергей Тыртычный <tyrtychnyy90@mail.ru>
2024-02-26 23:58:53 +0300

    Ok

--- cherry/settings.py
+++ cherry/settings.py
@@ -284,8 +284,8 @@ CELERY_TIMEZONE = 'Europe/Moscow'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('CHERRY_EMAIL')
-EMAIL_HOST_PASSWORD = os.environ.get('CHERRY_EMAIL_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
